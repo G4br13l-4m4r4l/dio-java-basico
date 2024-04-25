243727cc2d0f6547366bcff908e0d65e48a648e7
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-04-25 12:05:41 -0300

    Avancando na materia

---
+++ TiposDados/src/edu/gabriel/tiposVariavel/TiposDadosJava.java
@@ -0,0 +1,17 @@
+package edu.gabriel.tiposVariavel;
+public class TiposDadosJava {
+    public static void main(String[] args) throws Exception {
+        String meuNome = "Fulano";
+        int idade = 26;
+        double dinheiroNaConta = 25.00;
+
+        final double pi = 3.14;
+
+        System.out.println("Nome: ".concat(meuNome));
+        System.out.println("Idade: " + idade);
+        System.out.println("Saldo: " + dinheiroNaConta);
+        System.out.println("Valor de pi: " + pi);
+
+
+    }
+}
