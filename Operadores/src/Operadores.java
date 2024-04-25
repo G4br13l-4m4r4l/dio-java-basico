089d8391e8f02505126d840efca63ea597732095
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-04-25 15:52:27 -0300

    Avancando com operadores

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        String first = "first";
+        String second = "second";
+
+        if(first.equals(second)){
+            System.out.println("the exactly same expression!");
+        }else{
+            System.out.println("Not exactly the same");
+        }
+
+        boolean TrueOrFalse = true;
+
+        System.out.println(TrueOrFalse? "yep":"none");
+    }
+}
